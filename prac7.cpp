c2a26d1f133a0f3f915c5a040eec8d741e84baa1
parth263 <prathmeshpatil623@gmail.com>
2023-11-30 09:12:12 +0530

    updated

--- prac7.cpp
+++ prac7.cpp
@@ -94,3 +94,63 @@ int main()
     cout<<"\nThank You !";
     return 0;
 }
+
+
+
+
+
+// In C++, a "map" generally refers to an associative container provided by the C++ Standard Template Library (STL). The map is part of the C++ Standard Template Library (STL) and is a container that stores key-value pairs. It is implemented as a balanced binary search tree, typically a red-black tree.
+
+// Here are some key concepts related to the map container in C++:
+
+// 1. **Key-Value Pairs:**
+//    - A map stores elements in key-value pairs. Each element has a unique key associated with it, and the key is used to access the associated value. The combination of a key and its associated value is known as a "key-value pair."
+
+// 2. **Ordered Container:**
+//    - Maps in C++ are ordered containers, which means that the elements are stored in a specific order. The order is based on the keys, and it is typically sorted.
+
+// 3. **No Duplicate Keys:**
+//    - Each key in a map must be unique. If you try to insert a key that already exists in the map, the insertion operation will fail.
+
+// 4. **Search and Access:**
+//    - Searching for a specific key in a map is efficient. The map structure allows for fast lookups based on the keys.
+//    - Accessing the value associated with a key is also quick.
+
+// 5. **Insertion and Deletion:**
+//    - Elements (key-value pairs) can be inserted into a map using the `insert` function or using the `[]` operator.
+//    - Elements can be removed using the `erase` function.
+
+// 6. **Iterators:**
+//    - Maps support iterators that allow you to traverse the elements in order.
+
+// 7. **Balanced Binary Search Tree:**
+//    - The underlying implementation of a map is often a balanced binary search tree, typically a red-black tree. This ensures efficient search, insertion, and deletion operations.
+
+// Here's a simple example of using a map in C++:
+
+// ```cpp
+// #include <iostream>
+// #include <map>
+
+// int main() {
+//     // Creating a map with string keys and int values
+//     std::map<std::string, int> myMap;
+
+//     // Inserting key-value pairs
+//     myMap["Alice"] = 25;
+//     myMap["Bob"] = 30;
+//     myMap["Charlie"] = 22;
+
+//     // Accessing values using keys
+//     std::cout << "Age of Alice: " << myMap["Alice"] << std::endl;
+
+//     // Iterating through the map
+//     for (const auto& pair : myMap) {
+//         std::cout << pair.first << ": " << pair.second << std::endl;
+//     }
+
+//     return 0;
+// }
+// ```
+
+// In this example, `std::map<std::string, int>` is a map that uses strings as keys and integers as values. The program inserts key-value pairs into the map, accesses values using keys, and iterates through the map.
