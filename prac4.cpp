49115e5632817f5fc6256bf7f1ce92efac6d4f0e
parth263 <prathmeshpatil623@gmail.com>
2023-11-30 09:18:33 +0530

    Update prac4.cpp

--- prac4.cpp
+++ prac4.cpp
@@ -91,3 +91,49 @@ int main()
     cout<<"\nThank You !";
     return 0;
 }
+
+// File handling in C++ refers to the process
+//  of working with files, which allows you to read data from 
+//  files and write data to files. The C++ standard library provides 
+//  classes and functions for file input and output operations. 
+//  File handling in C++ is typically done using the <fstream> header, which provides
+//   the necessary classes for file operations.
+
+
+// In C++, `ofstream` and `ifstream` are classes provided by the Standard Template Library (STL) that are used for file input/output operations.
+
+// - **`ofstream` (Output File Stream):**
+//   - This class is used to create and write to files.
+//   - Objects of `ofstream` are used for output operations, such as writing data to a file.
+//   - It is part of the `<fstream>` header.
+
+//   Example:
+//   ```cpp
+//   ofstream fout;
+//   fout.open("output.txt");  // Opens a file named "output.txt" for writing
+//   fout << "Hello, World!";  // Writes data to the file
+//   fout.close();             // Closes the file
+//   ```
+
+// - **`ifstream` (Input File Stream):**
+//   - This class is used to read from files.
+//   - Objects of `ifstream` are used for input operations, such as reading data from a file.
+//   - It is part of the `<fstream>` header.
+
+//   Example:
+//   ```cpp
+//   ifstream fin;
+//   fin.open("input.txt");   // Opens a file named "input.txt" for reading
+//   string data;
+//   fin >> data;              // Reads data from the file
+//   fin.close();              // Closes the file
+//   ```
+
+// In the provided code you shared earlier:
+
+// ```cpp
+// ofstream fout;
+// ifstream fin;
+// ```
+
+// This code declares two objects, `fout` and `fin`, of the `ofstream` and `ifstream` classes, respectively. These objects are used for file output and input operations in the later part of the program. The program opens and closes files using these objects to write and read employee data.
